84b029b17cca52b1fa4701d1e4cd6c9a69c9ccb5
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-09 22:07:41 +0530

    Update tb_top.v

--- day2/encoder4*2/tb/tb_top.v
+++ day2/encoder4*2/tb/tb_top.v
@@ -1,1 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 11:01:28
+// Design Name: 
+// Module Name: encoder4_2tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module encoder4_2tb();
+reg [3:0]D;
+wire [1:0]b;
+integer m;
+initial
+begin
+
+end
+endmodule
 
