0e3135052ccbf7141fc9acd444c85a30b8da15f7
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-09 22:07:00 +0530

    Update top.v

--- day2/encoder4*2/design/top.v
+++ day2/encoder4*2/design/top.v
@@ -1,1 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 10:34:58
+// Design Name: 
+// Module Name: encoder4_2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module encoder4_2(input [3:0]D,output reg [1:0]b);
+always @(*)
+begin
+case(D)
+4'b0001: b=2'b00;
+4'b0010: b=2'b01;
+4'b0100: b=2'b10;
+4'b1000: b=2'b11;
+default: b = 2'b00;
+endcase
+end
+endmodule
 
