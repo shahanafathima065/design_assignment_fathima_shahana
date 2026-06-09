e4108e4c476a62da24a27f03776f4706d0ed0b15
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-09 22:08:16 +0530

    Update top.v

--- day2/sr_ff/design/top.v
+++ day2/sr_ff/design/top.v
@@ -1,1 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 13:37:02
+// Design Name: 
+// Module Name: sr_flipflop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+ module sr_flipflop(input s, r, rst, clk, output reg q, qbar);
+always@(posedge clk)
+begin
+    if(rst) begin        
+        q    <= 1'b0;
+        qbar <= 1'b1;
+    end
+    else if(s==0 && r==0) begin
+        q    <= q;        
+        qbar <= qbar;
+    end
+    else if(s==0 && r==1) begin
+        q    <= 1'b0;   
+        qbar <= 1'b1;
+    end
+    else if(s==1 && r==0) begin
+        q    <= 1'b1;     
+        qbar <= 1'b0;
+    end
+    else if(s==1 && r==1) begin
+        q    <= 1'bx;      
+        qbar <= 1'bx;
+    end
+end
+endmodule
+
 
