128afb3a029c4f6a3cbc3e666466854af405cd41
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-10 21:05:21 +0530

    Update tb_top.v

--- day3/sequence_detector/tb/tb_top.v
+++ day3/sequence_detector/tb/tb_top.v
@@ -1,1 +1,47 @@
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 15:40:24
+// Design Name: 
+// Module Name: task_1_sq_dt_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module task_1_sq_dt_tb();
+reg clk_tb,rst_tb,din_tb;
+wire detected_tb;
+task_1_sq_dt dut(clk_tb,rst_tb,din_tb,detected_tb);
+initial
+begin
+{clk_tb,rst_tb,din_tb}=0;
+end
+always #5 clk_tb=~clk_tb;
+initial
+begin
+rst_tb=1;
+#10;
+rst_tb=0;
+#10;
+din_tb=1;
+#10;
+din_tb=1;
+#10;
+din_tb=1;
+#10;
+din_tb=0;
+end
+endmodule
