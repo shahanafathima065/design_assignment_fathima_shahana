6bcb459c41f224db454923b2532449e1c3d7645e
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-23 06:00:17 +0530

    Update top.v

--- day3/sequence_detector/design/top.v
+++ day3/sequence_detector/design/top.v
@@ -1,82 +1,68 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.06.2026 11:48:53
-// Design Name: 
-// Module Name: task_1_sq_dt
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module task_1_sq_dt(input clk,rst,din,output reg detected);
-parameter idle=2'b00;
+module sequence_1110(input clk,rst,din,output reg detected
+
+    );
+parameter idle=2'b00;   
 parameter s1=2'b01;
 parameter s2=2'b10;
 parameter s3=2'b11;
-
 reg [1:0]ps,ns;
 //present state logic
 always @(posedge clk)
 begin
-if(rst)//synchrounous reset
-      begin
-      ps<=idle;
-      end 
-else
-   ps<=ns;
-end 
-
+if (rst)
+ps<=idle;//non blocking
+else 
+ps<=ns;//non blocking
+end
+// next state logic
 always @(*)
 begin
-    ns = ps;
-    detected = 0;
-
-    case(ps)
-
-    idle: begin
-        if(din==0)
-            ns = idle;
-        else
-            ns = s1;
-    end
-
-    s1: begin
-        if(din==0)
-            ns = idle;
-        else
-            ns = s2;
-    end
-
-    s2: begin
-        if(din==0)
-            ns = idle;
-        else
-            ns = s3;
-    end
-
-    s3: begin
-        if(din==0) begin
-            ns = idle;
-            detected = 1;
-        end
-        else
-            ns = s1;
-    end
-
-    endcase
+case(ps)
+idle:begin
+detected=1'b1;
+if(din==0) begin
+ns=idle;
+detected=1'b0;
+//blocking
 end
-
-
+else begin
+ns=s1;//blocking
+detected=1'b0;
+end
+end
+s1:begin
+if (din==0) begin
+ns=idle;
+detected=1'b0;
+end
+else begin
+ns=s2;
+detected=1'b0;
+end
+end
+s2:begin
+if (din==0) begin
+ns=idle;
+detected=1'b0;
+end
+else begin
+ns=s3;
+detected=1'b0;
+end
+end
+s3:begin
+if (din==0) begin
+ns=idle;
+detected=1'b1;
+end
+else begin
+ns=s3;
+detected=1'b0;
+end
+end
+endcase
+end
 endmodule
