09597cbce12c6b5a41a1468c92421d7090216237
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-23 11:18:56 +0530

    Create environment.sv

---
+++ AES_128_Optimization/Verification/environment.sv
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2026 21:17:10
+// Design Name: 
+// Module Name: environment
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+class environment;
+
+  generator  gen;
+  driver     drv;
+  monitor    mon;
+  scoreboard scb;
+
+  mailbox #(aes_transaction) gen2drv;
+  mailbox #(aes_transaction) drv2scb;
+  mailbox #(aes_transaction) mon2scb;
+
+  virtual aes_interface.DRIVER  drv_vif;
+  virtual aes_interface.MONITOR mon_vif;
+
+  function new(input virtual aes_interface.DRIVER drv_vif,
+               input virtual aes_interface.MONITOR mon_vif);
+
+    this.drv_vif = drv_vif;
+    this.mon_vif = mon_vif;
+
+    gen2drv = new();
+    drv2scb = new();
+    mon2scb = new();
+
+    gen = new(gen2drv);
+    drv = new(drv_vif, gen2drv, drv2scb);
+    mon = new(mon_vif, mon2scb);
+    scb = new(drv2scb, mon2scb);
+
+  endfunction
+
+  task run();
+
+    fork
+      gen.run();
+
+      begin
+        drv.reset_dut();
+        drv.run();
+      end
+
+      mon.run();
+      scb.run();
+    join_none
+
+  endtask
+
+endclass
