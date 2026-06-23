3af3c0d63b70827e7a7c38791281dd77029dc21d
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-23 07:05:16 +0530

    Update top.v

--- day5/BCDadder_withinterface/design/top.v
+++ day5/BCDadder_withinterface/design/top.v
@@ -1,1 +1,30 @@
-
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.06.2026 17:36:38
+// Design Name: 
+// Module Name: BCD_adder_using_ripplecarryadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module BCD_adder_using_ripplecarryadder(input [3:0]A_BCD,[3:0]B_BCD,cin_BCD,output [3:0]S_BCD,output cout_BCD);
+wire [3:0]w;
+wire w5,w6,w7,w8,w9;
+ripplecarryadder RC1(A_BCD,B_BCD,cin_BCD,w,w7);
+and(w5,w[3],w[2]);
+and(w6,w[3],w[1]);
+or(w8,w5,w6,w7);
+ripplecarryadder RC2(w,{1'b0,w8,w8,1'b0},1'b0,S_BCD,w9);
+assign cout_BCD = w8;
+endmodule
