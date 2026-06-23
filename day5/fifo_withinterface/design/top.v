cc31147d80578b0fded7c34b940d89128404aea4
shahanafathima065 <shahanafathima065@gmail.com>
2026-06-23 07:54:22 +0530

    top.v

--- day5/fifo_withinterface/design/top.v
+++ day5/fifo_withinterface/design/top.v
@@ -1,1 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 12:06:49
+// Design Name: 
+// Module Name: FIFO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module FIFO(
+    input clk,
+    input rst,
+    input wrenb,
+    input rdenb,
+    input [7:0] data_in,
+    output reg [7:0] data_out,
+    output full,
+    output empty
+);
+
+    reg [7:0] mem [7:0];
+    reg [2:0] rd_ptr = 0;
+    reg [2:0] wr_ptr = 0;
+    integer i;
+
+    assign full  = ((wr_ptr + 3'b001) == rd_ptr) ? 1'b1 : 1'b0;
+    assign empty = (wr_ptr == rd_ptr) ? 1'b1 : 1'b0;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            for (i = 0; i < 8; i = i + 1)
+                mem[i] <= 0;
+        end
+        else if (wrenb == 1 && full == 0) begin
+            mem[wr_ptr] <= data_in;
+            wr_ptr <= wr_ptr + 1;
+        end
+        else if (rdenb == 1 && empty == 0) begin
+            data_out <= mem[rd_ptr];
+            rd_ptr <= rd_ptr + 3'b001;
+        end
+    end
+
+endmodule
